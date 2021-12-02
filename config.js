2cd2849ec09248af740a02dd4a058ca230a9d2e4
amitca71 <amitca71@gmail.com>
2021-12-02 11:13:11 +0200

    ...

--- config.js
+++ config.js
@@ -1,7 +1,13 @@
 
+const { Kafka } = require('kafkajs');
 const kafka = {
-  clientId: "npm-slack-notifier",
+  clientId: process.env.KAFKA_CLIENT_NAME || "npm-sample-producer",
+// AMIT  brokers: [process.env.BOOTSTRAP_BROKER || "localhost:9092", process.env.BOOTSTRAP_BROKER2 || "", process.env.BOOTSTRAP_BROKER3 || ""], verify the best way to do it
   brokers: [process.env.BOOTSTRAP_BROKER || "localhost:9092"],
+  retry: {
+    initialRetryTime: 100,
+    retries: 8
+  },
   ssl: process.env.KAFKA_SSL ? JSON.parse(process.env.KAFKA_SSL) : false,
   sasl:
     process.env.KAFKA_USERNAME && process.env.KAFKA_PASSWORD
@@ -18,11 +24,16 @@ const consumer = {
 };
 
 const app = {
-  topic: process.env.TOPIC || "sample-topic"
+  topic: process.env.TOPIC || "sample-topic-17",
+  compression: process.env.KAFKA_COMPRESSION || Kafka.None  //other built in option in Kafka.GZIP
+};
+const producer = {
+  allowAutoTopicCreation: process.env.KAFKA_ALLOW_AUTO_TOPIC_CREATION || false
 };
 
 module.exports = {
   kafka,
   consumer,
+  producer,
   app
 };
--- producer.js
+++ producer.js
@@ -2,21 +2,21 @@ const { Kafka } = require('kafkajs');
 const config = require("./config");
 
 const value = process.argv[2] || 'Hello message !!';
-const partition = process.argv[3] || 0;
 
 run();
 async function run() {
     try {
         const kafka = new Kafka(config.kafka);
-        const producer = kafka.producer();
+        console.log('bfore  . . .');
+        const producer = kafka.producer(config.producer);
         console.log('Connecting to Kafka . . .');
         await producer.connect();
         console.log('Connected to Kafka !!');
 
         const result = await producer.send({
             topic: config.app.topic,
-//            messages: [{ value, partition }]
-            messages: [{ value}]
+            messages: [{ value}],
+            compression: config.app.compression
         });
 
         console.log(`Sent Successfully! ${JSON.stringify(result)}`);
@@ -24,5 +24,6 @@ async function run() {
     }
     catch (err) {
         console.error(`ERROR::PRODUCER:: ${err}`);
+//Amit need to  producer.disconnect(); so it would not be hanged- how?       
     }
 }
